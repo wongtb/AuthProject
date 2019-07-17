0d4c49e9d551c381c1a54c02de8cfa0460b7a2e0
Brian Wong <wongtb@mcmaster.ca>
2019-07-17 00:18:29 -0400

    fixes to various api calls, now working with /register, /me

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -34,14 +34,20 @@ router.post('/register', function(req, res){
 })
 
 // decode api
-router.get('me', function(req, res) {
+router.get('/me', function(req, res) {
     var token = req.headers['x-access-token'];
     if(!token) return res.status(401).send({ auth : false, message: 'No token provided.'});
 
     jwt.verify(token, config.secret, function(err, decoded){
         if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
 
-        res.status(200).send(decoded); 
+        //res.status(200).send(decoded); 
+        User.findById(decoded.id, { password: 0 }, function (err, user) {
+            if (err) return res.status(500).send("There was a problem finding the user.");
+            if (!user) return res.status(404).send("No user found.");
+            
+            res.status(200).send(user);
+          });
     });
 });
 
